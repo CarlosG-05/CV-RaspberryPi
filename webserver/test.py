7af71fc6c6c0717191815cd3004b56ff1d05428e
Carlos Guerrero <cguerrero.cg20@gmail.com>
2025-11-04 22:02:31 -0800

    Finished Test Route, Can Start To Develop Production Routes To SupaBase

--- webserver/test.py
+++ webserver/test.py
@@ -1,8 +1,13 @@
 import requests
+from datetime import datetime
 
 data = {
     "room_number": 101,
-    "current_occupancy": 5
+    "building": "Main Hall",
+    "capacity": 20,
+    "current_occupancy": 5,
+    "last_updated": datetime.now().isoformat()
 }
+
 response = requests.post("http://localhost:8000/data", json=data)
 print(response.json())
