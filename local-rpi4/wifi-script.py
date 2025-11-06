174fad18ed57cfe76e71a6120d209211568ac2b9
ElijahInamarga <elijahcek@gmail.com>
2025-11-06 10:55:15 -0800

    successful wifi connection

---
+++ local-rpi4/wifi-script.py
@@ -0,0 +1,22 @@
+import subprocess
+
+#TODO: Move to .env
+ssid = "SETUP-E811"
+password = "block9357bucket"
+
+add_command = ["nmcli", "connection", "add", "type", "wifi", "con-name", ssid, "ifname", "wlan0","ssid", ssid, "wifi-sec.key-mgmt", "wpa-psk", "wifi-sec.psk", password]
+
+activate_command = ["nmcli", "connection", "up", ssid]
+
+try:
+    print(f"Attempting to connect to {ssid}...")
+    subprocess.run(add_command, check=True)
+    print("Connection added")
+
+    print("Activating connection...")
+    subprocess.run(activate_command, check=True)
+    print("Successfully connected")
+except subprocess.CalledProcessError:
+    print("Connection failed")
+except FileNotFoundError:
+    print("nmcli command not found")
