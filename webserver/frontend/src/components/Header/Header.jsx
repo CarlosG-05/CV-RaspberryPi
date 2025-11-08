bf3da579cf20c61af700bae3592a0b9a559a8867
ElijahInamarga <elijahcek@gmail.com>
2025-11-08 12:41:21 -0800

    Basic header implementation

---
+++ webserver/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <>
+      <div class="container">
+        <h1 class="title">OccupyAI</h1>
+        <h1>OccupyAI</h1>
+        <h1>OccupyAI</h1>
+        <h1>OccupyAI</h1>
+        <h1>OccupyAI</h1>
+        <h1>OccupyAI</h1>
+      </div>
+    </>
+  )
+}
+
