f82d1178cb8a30da510ea53de468b14ee16e5adf
ElijahInamarga <elijahcek@gmail.com>
2025-11-08 12:53:41 -0800

    Added floor buttons

--- webserver/frontend/src/components/Header/Header.jsx
+++ webserver/frontend/src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
     <>
       <div class="container">
         <h1 class="title">OccupyAI</h1>
-        <h1>OccupyAI</h1>
-        <h1>OccupyAI</h1>
-        <h1>OccupyAI</h1>
-        <h1>OccupyAI</h1>
-        <h1>OccupyAI</h1>
+        <button>Floor 1</button>
+        <button>Floor 2</button>
+        <button>Floor 3</button>
+        <button>Floor 4</button>
+        <button>Floor 5</button>
       </div>
     </>
   )
