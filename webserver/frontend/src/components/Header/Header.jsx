64ea8ca724e448500249398cd48e567ba73c190c
ElijahInamarga <elijahcek@gmail.com>
2025-11-08 22:30:19 -0800

    Relative path not needed. Public directory considered root

--- webserver/frontend/src/components/Header/Header.jsx
+++ webserver/frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <div class="container">
-        <img class="title" src="../../../public/logo.png" alt="logo" width="200px" height="200px"/> 
+        <img class="title" src="/logo.png" alt="logo" width="200px" height="200px"/> 
         <h1>Love Library Study Rooms</h1>
         <button>1</button>
         <button>2</button>
